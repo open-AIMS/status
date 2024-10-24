1e2902620125f0f42c189b9ae6f877878ed349b5
Murray Logan <i.obesulus@gmail.com>
2024-10-25 06:27:51 +1000

    Add current item to status settings

--- R/status.R
+++ R/status.R
@@ -36,6 +36,7 @@ status_initialize <- function(pkgs = NULL, project_name = "Sediment Quality Anal
           log_file = list(item = basename(log_file), name = "Log filename"),
           time = list(item = get_current_time(), name = "Date/Time"),
           current_stage = list(item = 1, name = "Current stage")
+          current_item = list(item = 0, name = "Initialising")
           )
 
         status_ <- list(
