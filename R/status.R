7268ec308f297cc46bd7789391dd00acd6c3cee7
Murray Logan <i.obesulus@gmail.com>
2024-10-25 08:22:56 +1000

    debug

--- R/status.R
+++ R/status.R
@@ -647,7 +647,7 @@ add_status_item <- function(stage, order = NULL, item, name, status = "pending")
   ##  which(status_$status[[stage]]$status == "pending"))) - 1
   print(status_$status[[stage]]$items)
  print(status_$settings$current_item)
-  wch_order <- which(status_$status[[stage]]$items == status_$settings$current_item)
+  wch_order <- which(status_$status[[stage]]$items == status_$settings$current_item$item)
   print(wch_order)
   if (!is.null(order)) wch_order <- order
   ## need to slot the current item in before that
